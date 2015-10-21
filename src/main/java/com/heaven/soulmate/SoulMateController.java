fb90de3cfd700d5312d1f66dc8a966f248908437
chenjie3 <chenjie3@kingsoft.com>
2015-10-21 18:49:26 +0800

    make spring mvc work

---
+++ src/main/java/com/heaven/soulmate/SoulMateController.java
@@ -0,0 +1,21 @@
+package com.heaven.soulmate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ChenJie3 on 2015/9/8.
+ */
+@Controller
+public class SoulMateController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public Object test(HttpServletRequest request, HttpServletResponse response) {
+        return "hello soulmate & spring mvc!";
+    }
+}
