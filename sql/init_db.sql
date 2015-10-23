4b5b3308cf9801bbbd51f61ee10100095418eeb0
legol <legol.chen@gmail.com>
2015-10-24 01:41:05 +0800

    use local mysql

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,3 +1,6 @@
+
+use soulmate;
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
    `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -13,8 +16,8 @@ CREATE TABLE `user` (
 
 DROP TABLE IF EXISTS `relation`;
 CREATE TABLE `relation` (
-`uid` BIGINT(20) NULL COMMENT '',
-`friend_id` BIGINT(20) NULL COMMENT '',
+`uid` BIGINT(20) NOT NULL COMMENT '',
+`friend_id` BIGINT(20) NOT NULL COMMENT '',
 `memo` VARCHAR(255) NULL COMMENT '',
 `version` BIGINT(20) NULL COMMENT '',
 PRIMARY KEY(`uid`, `friend_id`)
@@ -23,8 +26,8 @@ PRIMARY KEY(`uid`, `friend_id`)
 
 DROP TABLE IF EXISTS `relation_reverse`;
 CREATE TABLE `relation_reverse` (
-`uid` BIGINT(20) NULL COMMENT '',
-`belongs_to_uid` BIGINT(20) NULL COMMENT '',
+`uid` BIGINT(20) NOT NULL COMMENT '',
+`belongs_to_uid` BIGINT(20) NOT NULL COMMENT '',
 `version` BIGINT(20) NULL COMMENT '',
 PRIMARY KEY(`uid`, `belongs_to_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -42,7 +45,7 @@ KEY(`sent_time`)
 
 DROP TABLE IF EXISTS `login_status`;
 CREATE TABLE `login_status` (
-`uid` BIGINT(20) NULL COMMENT '',
+`uid` BIGINT(20) NOT NULL COMMENT '',
 `token` VARCHAR(255) NULL COMMENT '',
 `last_login_time` TIMESTAMP,
 location Point NOT NULL COMMENT 'current location',
