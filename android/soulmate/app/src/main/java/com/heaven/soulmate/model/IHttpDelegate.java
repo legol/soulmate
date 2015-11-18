6729391deadce25c4f66531a78dad4440d76946d
chenjie3 <chenjie3@kingsoft.com>
2015-11-18 14:42:35 +0800

    IHttpDelegate

---
+++ android/soulmate/app/src/main/java/com/heaven/soulmate/model/IHttpDelegate.java
@@ -0,0 +1,8 @@
+package com.heaven.soulmate.model;
+
+/**
+ * Created by ChenJie3 on 2015/11/17.
+ */
+public interface IHttpDelegate {
+    public void onHttpResponse(HttpRequestData request, HttpResponseData response);
+}
