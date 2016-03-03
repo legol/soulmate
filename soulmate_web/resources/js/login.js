4b806d10d72dae202dcdb69c510f9e2d1216af43
chenjie3 <chenjie3@kingsoft.com>
2016-03-03 17:22:14 +0800

    add login callback. add switching to lobby.html

--- soulmate_web/resources/js/login.js
+++ soulmate_web/resources/js/login.js
@@ -5,12 +5,12 @@ if (!LoginController) {
     var LoginController = function () {
     };
 
-    LoginController.prototype.login = function (user_name, pwd) {
+    LoginController.prototype.login = function (user_name, pwd, callback) {
         alert("LoginController: login button clicked");
 
-        var testData = {
-            "phone":"15011113304",
-            "password":"803048"
+        var login_data = {
+            "phone":user_name,
+            "password":pwd
         };
 
         $.ajax({
@@ -20,16 +20,32 @@ if (!LoginController) {
             type: 'POST',
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            data: JSON.stringify(testData),
+            data: JSON.stringify(login_data),
             success: function (data) {
                 // process data
                 alert("success");
                 alert(JSON.stringify(data));
+
+                if (callback)
+                {
+                    var login_result = {
+                        "status": 0,
+                        "data":data
+                    };
+                    callback(login_result);
+                }
             },
             error: function(url, options){
                 alert("error occured");
                 alert(url.toString());
                 alert(options.toString());
+                if (callback)
+                {
+                    var login_result = {
+                        "status": -1
+                    };
+                    callback(login_result);
+                }
             }
         });
 
--- soulmate_web/resources/js/util/uicontroller.js
+++ soulmate_web/resources/js/util/uicontroller.js
@@ -22,9 +22,17 @@ if (!UIController) {
             },
 
             submit: function(){
-                window.logincontroller.login("hello");
+                window.logincontroller.login("15011113304", "803048", function(login_result){
+                    alert("login callback");
+                    alert(JSON.stringify(login_result));
+
+                    var r = new Render();
+                    r.render($("#main_container"), "resources/templates/chat/lobby.html", JSON.stringify(login_result));
+                });
             }
-        }
+        },
+
+
     };
 
     window.uicontroller = new UIController();
