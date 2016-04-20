e471779da1d0514217ed3a51f19a70c4a5a6744e
chenjie3 <chenjie3@kingsoft.com>
2016-04-20 16:46:32 +0800

    input.html will call chat/broadcast

--- soulmate_web/resources/js/controller/inputcontroller.js
+++ soulmate_web/resources/js/controller/inputcontroller.js
@@ -22,6 +22,33 @@ if (!InputController) {
             var log = log4javascript.getDefaultLogger();
             log.info("send message...");
 
+            var chat_data = new Object();
+
+            $.ajax({
+                url: "/chat/broadcast", // 不能跨域，只能访问自己本站内容
+                async: true,
+                timeout: 10000, // ms
+                type: 'POST',
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: JSON.stringify(chat_data),
+                success: function (response) {
+                    // process data
+                    var log = log4javascript.getDefaultLogger();
+                    log.info("chat success");
+                    log.info(JSON.stringify(response));
+
+                },
+                error: function(url, options){
+                    var log = log4javascript.getDefaultLogger();
+                    log.info("chat error occured");
+                    log.info(url.toString());
+                    log.info(options.toString());
+
+                }
+            });
+
+
         },
 
     }
