bb40d82e8b67ae26c5a09686b8d2e43da3a6af5e
chenjie3 <chenjie3@kingsoft.com>
2016-05-04 18:24:23 +0800

    fileuploader via ajax

---
+++ fileuploader_ajax/src/main/java/com/heaven/soulmate/fileuploader_ajax/Utils.java
@@ -0,0 +1,141 @@
+package com.heaven.soulmate.fileuploader_ajax;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Timestamp;
+import java.util.Properties;
+import java.util.Random;
+
+/**
+ * Created by ChenJie3 on 2015/10/23.
+ */
+public class Utils {
+
+    static Random rand = new Random();
+
+    // read a property file from resource folder
+    static public Properties readProperties(String filename){
+        Properties props = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = loader.getResourceAsStream(filename);
+        try {
+            props.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return props;
+    }
+
+    public static String md5(String original) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        md.update(original.getBytes());
+        byte[] digest = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+
+        return sb.toString();
+    }
+
+    public static int randInt(int min, int max) {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
+    public static String generateToken(long uid, String password) {
+        String secret = "chenjie love cxf!";
+        long rand = randInt(19800202, 19901116);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        String token_raw = String.format("%d %d %s %s %d", uid, rand, secret, password, timestamp.getTime());
+        return md5(token_raw);
+    }
+
+    // HTTP GET request
+    public static String httpGet(String url){
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        String result = null;
+
+        // add request header
+        request.addHeader("User-Agent", "soulmate");
+
+        HttpResponse response = null;
+        try {
+            response = client.execute(request);
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+
+            result = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // post json to url
+    public static String httpPost(String url, String json){
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+        String result = null;
+
+        // add header
+        post.setHeader("User-Agent", "soulmate");
+
+        StringEntity requestEntity = new StringEntity(
+                json,
+                ContentType.APPLICATION_JSON);
+        post.setEntity(requestEntity);
+
+        HttpResponse response = null;
+        try {
+            response = client.execute(post);
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+
+            result = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
---
+++ fileuploader_ajax/src/main/java/com/heaven/soulmate/fileuploader_ajax/controller/FileUploaderAjaxController.java
@@ -0,0 +1,85 @@
+package com.heaven.soulmate.fileuploader_ajax.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heaven.soulmate.fileuploader_ajax.Utils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by ChenJie3 on 2015/9/8.
+ */
+@Controller
+public class FileUploaderAjaxController {
+    private static final Logger LOGGER = Logger.getLogger(FileUploaderAjaxController.class);
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Object test(HttpServletRequest request, HttpServletResponse response) {
+        return "hello world";
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(HttpServletRequest request, @RequestParam("file") MultipartFile[] files) {
+
+        // Root Directory.
+        Properties fileuploadProperties = Utils.readProperties("fileupload.properties");
+        String uploadRootPath = fileuploadProperties.getProperty("root_dir");
+        LOGGER.info("uploadRootPath=" + uploadRootPath);
+
+        File uploadRootDir = new File(uploadRootPath);
+        //
+        // Create directory if it not exists.
+        if (!uploadRootDir.exists()) {
+            uploadRootDir.mkdirs();
+        }
+        //
+        List<File> uploadedFiles = new ArrayList<File>();
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+
+            // Client File Name
+            String name = file.getOriginalFilename();
+            System.out.println("Client File Name = " + name);
+
+            if (name != null && name.length() > 0) {
+                try {
+                    byte[] bytes = file.getBytes();
+
+                    // Create the file on server
+                    File serverFile = new File(uploadRootDir.getAbsolutePath()
+                            + File.separator + name);
+
+                    // Stream to write data to file in server.
+                    BufferedOutputStream stream = new BufferedOutputStream(
+                            new FileOutputStream(serverFile));
+                    stream.write(bytes);
+                    stream.close();
+                    //
+                    uploadedFiles.add(serverFile);
+                    System.out.println("Write file: " + serverFile);
+                } catch (Exception e) {
+                    System.out.println("Error Write file: " + name);
+                }
+            }
+        }
+
+        return "hello";
+    }
+}
